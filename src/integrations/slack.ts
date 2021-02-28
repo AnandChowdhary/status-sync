67423136b88743201768f94b2a4d2b0bcb1c2890
Anand Chowdhary <mail@anandchowdhary.com>
2021-02-28 12:50:08 +0530

    :sparkles: Add setting Slack status

---
+++ src/integrations/slack.ts
@@ -0,0 +1,15 @@
+import { WebClient } from "@slack/web-api";
+import type { Status } from "../interfaces";
+
+const token = process.env.SLACK_TOKEN;
+const slack = new WebClient(token);
+
+export const updateSlackStatus = async (status: Status) => {
+  await slack.users.profile.set({
+    status_text: status.text,
+    status_emoji: status.emoji,
+    status_expiration: status.expiration
+      ? Math.floor(status.expiration.getTime() / 1000)
+      : 0,
+  });
+};
