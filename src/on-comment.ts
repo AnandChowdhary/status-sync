f2ec0e007cf47b3ea90308936115e2c94a9cbe21
Anand Chowdhary <mail@anandchowdhary.com>
2021-02-28 14:44:14 +0530

    :loud_sound: Log issue context

--- src/on-comment.ts
+++ src/on-comment.ts
@@ -14,6 +14,7 @@ export const onIssueComment = async ({
 }) => {
   const oneHourAgo = new Date();
   oneHourAgo.setHours(oneHourAgo.getHours() - 1);
+  console.log(JSON.stringify(context.issue, null, 2));
 
   const { data } = await octokit.issues.listComments({
     owner: context.repo.owner,
